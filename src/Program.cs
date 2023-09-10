68daf51d8dfdf267c5c447bfe60a58b73f37c3c5
uniblab <uniblab@hotmail.com>
2023-09-10 19:05:57 -0400

    updating copyright & license

--- src/Program.cs
+++ src/Program.cs
@@ -110,7 +110,6 @@ namespace Icod.Preamble {
 				"Preamble.exe prepends the specified string to the head of the specified file.",
 				"Copyright (C) 2023 Timothy J. Bruce",
 				"",
-				"",
 				"This program is free software: you can redistribute it and / or modify",
 				"it under the terms of the GNU General Public License as published by",
 				"the Free Software Foundation, either version 3 of the License, or",
